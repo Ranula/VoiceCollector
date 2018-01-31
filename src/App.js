1fb5a69b51553c8c673b017f80fa5dbbae8e44c3
sudeepa14 <sudeepa.14@cse.mrt.ac.lk>
2018-01-31 06:32:28 +0000

    added the basic header and the recorder

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Demo from './recorder/recorder2.js'
+// import ReactDOM from 'react-dom';
 
 class App extends Component {
   render() {
     return (
+
+ 
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <h1 className="App-title">Welcome to React</h1> */}
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>පහත මෙනුවෙන් විධානයක් තෝරන්න</p>
+        
+        <select>
+          <option value="volvo">ශේෂය කොපමනද?</option>
+          <option value="saab">අවසන් ගනුදෙනු පහ පෙන්වන්න </option>
+          <option value="mercedes">පින් අංකය වෙනස් කරන්න</option>
+          <option value="audi">පසු මෙනුවට </option>
+        </select>
+      <Demo />
       </div>
+       
     );
   }
 }
+// ReactDOM.render(<recorder />, document.getElementById('App'));
 
 export default App;
